19ec6ab37deb53d8a1bd74f538ade36243ece115
Millie Jackson <100158073+Millie-Jackson@users.noreply.github.com>
2024-05-09 16:42:43 +0100

    reads credentials and creates an engine

--- database_utils.py
+++ database_utils.py
@@ -1,6 +1,9 @@
 # database_utils.py
 
+import psycopg2
+import sqlalchemy
 import yaml
+from sqlalchemy import engine, create_engine
 
 class DatabaseConnector:
     '''Upload and connect to the database'''
@@ -18,11 +21,38 @@ class DatabaseConnector:
         except yaml.YAMLError as e:
             print("Error parsing YAML:", e)
             return {}
+        
+    def init_db_engine(self, file_path):
+        '''Reads credentials and returns SQLAlchemy database engine'''
+
+        # Read credentials
+        creds = self.read_db_creds(file_path)
+
+        # Construct database URL
+        db_url = f"postgresql://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}"
+
+        try:
+            # Create engine
+            engine = create_engine(db_url)
+
+            # Test connection
+            with engine.connect():
+                pass
+        except Exception as e:
+            print("Error creating engin:", e)
+            return None
+
+        return engine
 
 
 
 if __name__ == "__main__":
 
     db_connector = DatabaseConnector()        
-    creds_dict = db_connector.read_db_creds('db_creds.yaml')
-    print(creds_dict)
+    engine = db_connector.init_db_engine('db_creds.yaml')
+    print(engine)
+
+    if engine:
+        print("Engine created successfully:", engine)
+    else:
+        print("Failed to create engine") 
