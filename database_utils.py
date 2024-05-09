7d9003c038a2757944d0bbb724dd771bb78d4f88
Millie Jackson <100158073+Millie-Jackson@users.noreply.github.com>
2024-05-09 15:49:09 +0100

    Wrote, read in and returns a dictionary for the database credentials

--- database_utils.py
+++ database_utils.py
@@ -1,6 +1,28 @@
 # database_utils.py
 
+import yaml
+
 class DatabaseConnector:
     '''Upload and connect to the database'''
 
-    def __init__():
+    def read_db_creds(self, file_path):
+        '''Reads and returns dictionary of credentials'''
+        
+        try:
+            with open(file_path, 'r') as file:
+                creds = yaml.safe_load(file)
+            return creds
+        except FileNotFoundError:
+            print("Error: File (db_creds) not found")
+            return {}
+        except yaml.YAMLError as e:
+            print("Error parsing YAML:", e)
+            return {}
+
+
+
+if __name__ == "__main__":
+
+    db_connector = DatabaseConnector()        
+    creds_dict = db_connector.read_db_creds('db_creds.yaml')
+    print(creds_dict)
